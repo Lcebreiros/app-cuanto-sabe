93e336905b0db8443a03a271fb7ae37d97ab36b5
Lcebreiros <leancebreiros@hotmail.com>
2025-10-09 12:26:30 -0300

    modificacion de tabla user

---
+++ database/migrations/2025_10_09_152432_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('dni_ultimo4'); // Hash de los últimos 4 dígitos del DNI
+            $table->string('role')->default('user'); // Rol del usuario
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
